26f2ae9edb8208462290d57f123aad85c8d5ad00
Petunia <psofia20056@gmail.com>
2025-02-18 17:44:15 +0000

    to-do list

---
+++ assets/scripts/sortList.js
@@ -0,0 +1,13 @@
+todoList = {
+  name: 'wash dishes',
+  date: '2022/12/13'
+}
+
+const inputName = document.querySelector('js-name');
+const inputDate = document.querySelector('js-date');
+const addBtn = document.querySelector('js-add-btn');
+
+function addTodo() {
+  addBtn.addEventListener('click', () => {
+  });
+}
