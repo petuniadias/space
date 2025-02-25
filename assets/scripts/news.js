50a8ec795789f5f2dc908404b73aff78590773df
Petunia <psofia20056@gmail.com>
2025-02-25 22:44:19 +0000

    descrição aparece quando dá hover, mudança de grid para flex

--- assets/scripts/news.js
+++ assets/scripts/news.js
@@ -1,8 +1,16 @@
 function displayDescription() {
-  const newsDescription = document.querySelector('news-description');
   document.querySelectorAll('.news-box').forEach(box => {
+    const newsDescription = box.querySelector('.news-description');
+    const newsTitle = box.querySelector('.news-title');
+
+    box.addEventListener('mouseenter', () => {
+      newsDescription.style.display = 'block';
+      newsTitle.style.bottom = `60px`;
+    });
     box.addEventListener('mouseleave', () => {
       newsDescription.style.display = 'none';
+      newsTitle.style.bottom = '0px';
     });
   });
 }
+displayDescription();
